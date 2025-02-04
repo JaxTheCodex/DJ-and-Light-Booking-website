40adef73177c3a2b815826b5cf30a104fe59720d
JaxTheCodex <jagarnathpatro836@gmail.com>
2025-02-04 20:19:50 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Responsive Navigation Menu
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('menu').classList.toggle('hidden');
+});
+
+document.getElementById('menu-close').addEventListener('click', function() {
+    document.getElementById('menu').classList.add('hidden');
+});
+
+
